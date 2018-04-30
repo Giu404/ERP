0206ef0fc36e59621ebc4e3a996c0a223ef1c37f
Giuliano Gozza <ggozza@stud.hs-heilbronn.de>
2018-04-30 14:20:07 +0200

    Move status text from sysout to UI

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Main extends Application implements EventHandler<KeyEvent>{
@@ -17,14 +19,13 @@ public class Main extends Application implements EventHandler<KeyEvent>{
 	private static TextField nameField;
 	private static PasswordField passwordField;
 	private static Button button;
+	private static Label statusLabel;
 	private static SAPController sapController;
 
 	public static void main(String[] args) throws JCoException {
 		sapController = new SAPController();
 		launch(args);
 	}
-	
-
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -35,17 +36,25 @@ public class Main extends Application implements EventHandler<KeyEvent>{
         passwordField.setPromptText("Passwort");
         button = new Button();
         button.setText("Login");
+        statusLabel = new Label();
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
             	boolean loginSuccess = sapController.tryLogin(nameField.getText(), passwordField.getText());
-    			System.out.println(loginSuccess ? "Login successful!" : "Login NOT successful!");
+    			if(loginSuccess){
+    				statusLabel.setText("Login successful!");
+    				statusLabel.setTextFill(Paint.valueOf("green"));
+    			} else {
+    				statusLabel.setText("Login NOT successful!");
+    				statusLabel.setTextFill(Paint.valueOf("red"));
+    			}
             }
         });
         
         root.getChildren().add(nameField);
         root.getChildren().add(passwordField);
         root.getChildren().add(button);
+        root.getChildren().add(statusLabel);
         stage.setTitle("Logistik SAP Tool");        
         Scene scene = new Scene(root, 400, 400);
         scene.setOnKeyPressed(this);
